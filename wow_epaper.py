69427901a93e268b383466e9ec9c9fad06f8255c
curarin <145868208+curarin@users.noreply.github.com>
2023-10-06 19:54:49 +0200

    Upload plotting and epaper scripts

---
+++ wow_epaper.py
@@ -0,0 +1,162 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import time
+import sys
+import epaper
+from PIL import Image, ImageDraw, ImageFont, ImageEnhance
+import sqlite3
+import os
+from gpiozero import Button
+from signal import pause
+import requests
+from io import BytesIO
+
+
+##### DRIVERS AND INITIALIZING DISPLAY########
+epd = epaper.epaper("epd7in5_V2").EPD() # get the display
+epd.init()           # initialize the display
+print("Clearing display...")    # prints to console, not the display, for debugging
+epd.Clear()      # clear the display
+
+### DEFINING BUTTONS
+key1 = Button(5)
+key2 = Button(6)
+
+##### FONTS #######
+font_content = ImageFont.truetype("/home/paulherzog/.fonts/folkard_.ttf", 25)  #folkard
+font_header = ImageFont.truetype("/home/paulherzog/.fonts/LifeCraft_Font.ttf", 60) #lifecraft
+
+### GET DB DATA ###
+connection_obj = sqlite3.connect("/home/paulherzog/sql_databases/jack_wow.db")
+table_name_char_data = "char_performance_data"
+cursor_obj = connection_obj.cursor()
+print("Connected to jack_wow.db.")
+schema_info = cursor_obj.fetchall()
+
+query = f"SELECT * FROM {table_name_char_data}"
+df_with_api_data = pd.read_sql_query(query, connection_obj)
+connection_obj.close()
+number_of_rows_in_df = len(df_with_api_data)
+current_row_selected = 0
+
+### DEFINING FUNCTIONS
+## for button action ###
+def button_a_callback():
+	global current_row_selected
+	current_row_selected = (current_row_selected + 1) % number_of_rows_in_df
+	data_current_row()
+
+def button_b_callback():
+	global current_row_selected
+	current_row_selected = (current_row_selected - 1) % number_of_rows_in_df
+	data_current_row()
+
+key1.when_pressed = button_a_callback
+key2.when_pressed = button_b_callback
+
+# for defining which class thumbnail to get
+def get_class_picture(char_class):
+	path = "/home/paulherzog/python/"
+	if char_class == "Warrior":
+		class_picture = "classicon_warrior.bmp"
+	elif char_class == "Druid":
+		class_picture = "classicon_druid.bmp"
+	elif char_class == "Warlock":
+		class_picture = "classicon_warlock.bmp"
+	elif char_class == "Monk":
+		class_picture = "classicon_monk.bmp"
+	elif char_class == "Shaman":
+		class_picture = "classicon_shaman.bmp"
+	elif char_class == "Paladin":
+		class_picture = "classicon_paladin.bmp"
+	elif char_class == "Evoker":
+		class_picture = "classicon_evoker.bmp"
+	elif char_class == "Demon Hunter":
+		class_picture = "classicon_demon_hunter.bmp"
+	elif char_class == "Death Knight":
+		class_picture = "classicon_death_knight.bmp"
+	elif char_class == "Mage":
+		class_picture = "classicon_mage.bmp"
+	elif char_class == "Rogue":
+		class_picture = "classicon_rogue.bmp"
+	elif char_class == "Priest":
+		class_picture = "classicon_priest.bmp"
+	elif char_class == "Hunter":
+		class_picture = "classicon_hunter.bmp"
+	else:
+		class_picture = "classicon_monk.bmp"
+	path_to_classicon = path + class_picture
+	return path_to_classicon
+
+# for getting plot data
+def get_raid_plot(char_name):
+	path = "/home/paulherzog/python/"
+	file_name = f"raid_overview_{char_name}.bmp"
+	full_path_to_plot = path + file_name
+	return full_path_to_plot
+
+## for storing data based on current button selection and pushing to epaper
+def data_current_row():
+	row_data_currently_selected = df_with_api_data.iloc[current_row_selected]
+	HImage = Image.new("1", (epd.width, epd.height), 255)
+	draw = ImageDraw.Draw(HImage)
+
+	#character data
+	char_name = row_data_currently_selected["charname"]
+	char_class = row_data_currently_selected["charclass"]
+	char_realm = row_data_currently_selected["charrealm"]
+	char_guild = row_data_currently_selected["charguild"]
+	char_realm = row_data_currently_selected["charrealm"]
+	char_ilevel = row_data_currently_selected["charilevel"]
+	char_rio = row_data_currently_selected["mplus_rio"]
+	char_wcl_performance_average = row_data_currently_selected["wcl_performance_average"]
+	raid_summ = row_data_currently_selected["raid_summary"]
+
+	#picture data
+	class_picture_bmp = get_class_picture(char_class)
+	img_bmp = Image.open(class_picture_bmp)
+	class_picture_bmp = img_bmp
+	HImage.paste(class_picture_bmp, (5, 5))
+
+	#misc data
+	date_last_api_fetch = row_data_currently_selected["date_data_fetched"]
+	print(f"Data for Character '{char_name}' in Row:", current_row_selected)
+
+	#here we are pushing all relevent informations #
+	draw.text((60, 5), char_name, font = font_header, fill = 0)
+	draw.text((300, 10), char_realm, font = font_content, fill = 0)
+	draw.text((300, 40), char_guild, font = font_content, fill = 0)
+	draw.text((460, 10), f"iLevel: {char_ilevel}", font = font_content, fill = 0)
+	draw.text((460, 40), f"RIO: {char_rio}", font = font_content, fill = 0)
+	draw.text((640, 10), f"Raid: {raid_summ}", font = font_content, fill = 0)
+	draw.text((640, 40), f"WCL:  {char_wcl_performance_average}", font = font_content, fill = 0)
+	#draw.text((600, 0), "Last Update: " + date_last_api_fetch, fill = 0)
+	#draw.text((600, 40), f"Character {current_row_selected}/{number_of_rows_in_df}", fill = 0)
+
+        #draw lines
+	draw.line([(5,70),(795,70)], fill = 0, width = 1)
+
+	#push plots
+	raid_plot_bmp = get_raid_plot(char_name)
+	raid_plot_bmp = Image.open(raid_plot_bmp)
+	raid_plot_bmp = raid_plot_bmp.resize((800, 400), Image.ANTIALIAS)
+	HImage.paste(raid_plot_bmp, (0, 80))
+
+	#push to display
+	epd.display(epd.getbuffer(HImage))
+	time.sleep(2)
+#################### GET ALL THE DATA FROM DATAFRAME
+
+######## PUSH FIRST VEW AFTER REBOOT
+data_current_row()
+
+###################################################
+try:
+	pause()
+
+except KeyboardInterrupt:
+	pass
+
+
+
---
+++ wow_plotting.py
@@ -0,0 +1,143 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sqlite3
+from PIL import Image, ImageEnhance
+import os
+
+### GET DB DATA ###
+connection_obj = sqlite3.connect("/home/paulherzog/sql_databases/jack_wow.db")
+table_name_char_data = "char_performance_data"
+cursor_obj = connection_obj.cursor()
+print("Connected to jack_wow.db.")
+schema_info = cursor_obj.fetchall()
+
+query = f"SELECT * FROM {table_name_char_data}"
+df_with_api_data = pd.read_sql_query(query, connection_obj)
+connection_obj.close()
+
+## FUNCTION FOR JPG TO BMP
+def img_convert(img_file,X_new,Y_new):
+        img = Image.open(img_file)
+        enhancer = ImageEnhance.Contrast(img)
+        contrast = 5
+        img = enhancer.enhance(contrast)
+        img = img.convert("1")
+        img = img.resize((X_new,Y_new), Image.LANCZOS)
+        return img
+
+
+
+########
+
+### LOOPING ###
+for char in df_with_api_data.itertuples(index=False):
+        char_name = char.charname
+        row_data_currently_selected = df_with_api_data[df_with_api_data["charname"] == char_name]
+        kills_data = row_data_currently_selected.filter(like='_totalkills')
+        encounter_numbers = df_with_api_data.filter(like='_totalkills')
+        encounter_count = len(encounter_numbers.columns)
+        rank_percent_data = row_data_currently_selected.filter(like='_rankpercent')
+        region_rank_data = row_data_currently_selected.filter(like='_regionrank')
+        rank_percent_data = rank_percent_data.replace('None', np.nan).astype(float)
+        encounter_names = [df_with_api_data[f'encounter_{i}_name'].iloc[0] for i in range(1, encounter_count + 1)]
+
+        region_ranks = [region_rank_data[f'encounter_{i}_regionrank'] for i in range(1, encounter_count + 1)]
+
+        x = np.arange(encounter_count)
+
+        bar_width = 0.45
+        print(f"Plotting starts for {char_name}...")
+        fig, ax1 = plt.subplots(figsize=(10, 6))
+        bars1 = ax1.bar(x, kills_data.iloc[0], width=bar_width, label='Total Kills', color='lightgray',hatch='////')  # Gray hatch pattern
+
+        ax1.set_xticks(x + bar_width)
+        encounter_names_split = ['\n'.join(name.split(' ', len(name) // 2)) for name in encounter_names]
+        ax1.set_xticklabels(encounter_names_split, rotation=45, ha="right", fontsize=10)
+
+        ax1.set_xlabel('Encounters')
+        ax1.set_ylabel('Total Kills')
+        ax1.set_title('Raid Performance Overview')
+#        ax1.legend(loc='upper left')
+
+        ax1.spines['top'].set_visible(False)
+        ax1.spines['right'].set_visible(False)
+        ax1.spines['bottom'].set_visible(False)
+        ax1.spines['left'].set_visible(False)
+        ax2 = ax1.twinx()
+        bars2 = ax2.bar(x + bar_width, rank_percent_data.iloc[0], width=bar_width, label='Top Ranking Percentile (in %)',color='black', hatch='\\\\\\\\')  # Black hatch pattern
+#        for bar in bars2:
+#                bar.set_linewidth(1.0)
+        ax2.set_ylabel('Top Ranking Percentile (in %)')
+#        ax2.legend(loc='upper right')
+
+        ax2.spines['top'].set_visible(False)
+        ax2.spines['right'].set_visible(False)
+        ax2.spines['bottom'].set_visible(False)
+        ax2.spines['left'].set_visible(False)
+
+        for bar1, bar2, encounter_name in zip(bars1, bars2, encounter_names):
+                height1 = bar1.get_height()
+                height2 = bar2.get_height()
+                ax1.annotate(f'{height1}', (bar1.get_x() + bar1.get_width() / 2, height1),
+                         ha='center', va='bottom', fontsize=10)
+                ax2.annotate(f'{height2:.2f}', (bar2.get_x() + bar2.get_width() / 2, height2),
+                        ha='center', va='bottom', fontsize=10)
+                region_rank = region_ranks[encounter_names.index(encounter_name)]
+                if (region_rank <= 3).any():
+                        bar_middle_x = bar2.get_x() + bar2.get_width() / 2
+                        star_y = height2 * 0.5
+                        ax2.annotate("""
+ .__.
+ (|  |)
+ (  )
+ _)(_ 
+TOP
+ 3
+
+ G
+ G
+ W
+ P
+
+
+""", (bar_middle_x, star_y), ha='center', va='center', fontsize=10, color='white')
+                if (region_rank > 3).any() & (region_rank <= 10).any():
+                        bar_middle_x = bar2.get_x() + bar2.get_width() / 2
+                        star_y = height2 * 0.5  # Adjust the y-coordinate to 50% of the bar's height
+                        ax2.annotate("""
+ .__.
+ (|  |)
+ (  )
+ _)(_ 
+TOP
+10
+
+""", (bar_middle_x, star_y), ha='center', va='center', fontsize=10, color='white')
+                elif (region_rank < 50).any() & (region_rank > 10).any():
+                        # Calculate the middle position of the bar and adjust the y-coordinate for the star
+                        bar_middle_x = bar2.get_x() + bar2.get_width() / 2
+                        star_y = height2 * 0.2  # Adjust the y-coordinate to 50% of the bar's height
+                        ax2.annotate("""
+ .__.
+ (|  |)
+ (  )
+ _)(_
+TOP
+50
+
+""", (bar_middle_x, star_y), ha='center', va='center', fontsize=10, color='white')
+
+ #       ax1.legend(loc='upper left', bbox_to_anchor=(0.05, 1.0))
+ #       ax2.legend(loc='upper left', bbox_to_anchor=(0.4, 1.0))
+
+        plt.tight_layout()
+        dpi = 240
+        temp_file = f'/home/paulherzog/python/raid_overview_{char_name}.png'
+        plt.savefig(temp_file, dpi=dpi, bbox_inches='tight', transparent=True)
+        #bmp_image = img_convert(temp_file, 800, 400)
+        bmp_image = Image.open(temp_file)
+        bmp_file = f'raid_overview_{char_name}.bmp'
+        bmp_image.save(bmp_file, dpi=(dpi, dpi))
+        os.remove(temp_file)
+        print(f"Raid Data Plotting completed for {char_name}...")
